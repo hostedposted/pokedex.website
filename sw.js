f3ddbe7e4be52287b4afe7e8e73579dd4edcff54
hostedposted <70211235+hostedposted@users.noreply.github.com>
2020-11-06 19:17:49 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', () => {
+    console.log(`installing service worker`);
+})
+
+self.addEventListener('activate', () => {
+    console.log(`activating service worker`);
+})
