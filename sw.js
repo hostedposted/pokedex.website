70b20e18cdb97a64c862135f2eab3f95e84215ed
hostedposted <70211235+hostedposted@users.noreply.github.com>
2020-12-08 15:48:19 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log("Opened cache 1.2.6");
+        console.log("Opened cache 1.2.7");
         return cache.addAll(urlsToCache);
       })
   );
